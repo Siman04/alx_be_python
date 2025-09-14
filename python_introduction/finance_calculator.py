4581eaec5c3d566ce14062ec7d5d76142bb646cb
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-09-14 21:50:47 +0000

    new finnance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,4 +8,16 @@ print(f"Your monthly savings are: ${monthly_savings}")
 annual_savings_no_interest = monthly_savings * 12
 projected_savings = annual_savings_no_interest * (1 + 0.05)
 print(f"Projected savings after one year, with interest, is ${projected_savings:.2f}")
+# Monthly Savings
+income = float(input("Enter your monthly income: "))
+expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = income - expenses
+print(f"Your monthly savings are: ${monthly_savings:.2f}")
+
+# Projected Annual Savings
+annual_savings_no_interest = monthly_savings * 12
+projected_savings = (monthly_savings * 12) * (1 + 0.05)
+
+print(f"Projected savings after one year, with interest, is ${projected_savings:.2f}")
+
 
