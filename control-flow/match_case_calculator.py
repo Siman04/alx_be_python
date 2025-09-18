0388001833a8862bdf42202d0eb1896e857aff2b
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-09-18 21:23:32 +0000

    actual new changed calc

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operator = input("Enter an operator (+, -, *, /): ")
+operator = input("Enter an operator (+, -, *, /):  ")
 if operator == '+':
     result = num1 + num2
     print("The result is {}.".format(result))
