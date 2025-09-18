6b7ea454493860913bbb083a945d50dc6dd3b553
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-09-18 21:17:14 +0000

    new culculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+
+if operator == '+':
+    result = num1 + num2
+    print("The result is {}.".format(result))
+elif operator == '-':
+    result = num1 - num2
+    print("The result is {}.".format(result))
+elif operator == '*':
+    result = num1 * num2
+    print("The result is {}.".format(result))
+elif operator == '/':
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        result = num1 / num2
+        print("The result is {}.".format(result))
+
+    
+     
+
