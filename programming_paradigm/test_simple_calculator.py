b86392fe212daf9082a81ee8d2a6b7ac66739b5a
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-10-04 19:53:58 +0000

    jh

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,7 +18,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-5, -2), -3)
         self.assertEqual(self.calc.subtract(0, 8), -8)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(5, -2), -10)
         self.assertEqual(self.calc.multiply(-6, -3), 18)
