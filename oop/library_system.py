c6056064ed048fb1e665cd8728455a9fa514499f
siman <simanahmed2004@gmail.com>
2025-10-11 18:28:21 +0300

    yesss againnnn

---
+++ oop/library_system.py
@@ -0,0 +1,41 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+    
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in KB
+
+    def __str__(self):
+        return f"Ebook: {self.title} by {self.author}, FileSize: {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"Added: {book}")
+
+    def list_books(self):
+        print("Library contains:")
+        for book in self.books:
+            print(f"- {book}")
+    
+    
+
+ 
