8fcbd8d3ce2710a63635eeb45f533394d0c1be52
siman <simanahmed2004@gmail.com>
2025-10-11 18:44:41 +0300

    yurr

--- oop/library_system.py
+++ oop/library_system.py
@@ -21,7 +21,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
 class Library:
     def __init__(self):
@@ -32,6 +32,6 @@ class Library:
        
 
     def list_books(self):
-        print("Library contains:")
+
         for book in self.books:
-            print(f" - {book}")
+            print(book)
