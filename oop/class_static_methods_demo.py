c715819b52466689f089fea779d6d0b2f1cffdca
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-10-11 17:51:02 +0000

    uuuu

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,10 +2,10 @@ class Calculator:
     calculation_type = "Arithmetic Operations"
 
     @staticmethod
-    def add(x, y):
-        return x + y
+    def add(a, b):
+        return a + b
 
     @staticmethod
-    def multiply(x, y):
+    def multiply(a, b):
         print(f"Calculation type: {Calculator.calculation_type}")
-        return x * y
+        return a * b
